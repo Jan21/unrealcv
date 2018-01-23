Camera rotation fix
Fixed camera horizontal and vertical rotation.
Sync commit

db_generation/camera_manipulation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from math import cos, sin, radians
-np.set_printoptions(precision=5, suppress=True)
+np.set_printoptions(precision=3, suppress=True)
 
 
 def get_translation_matrix(t):
@@ -13,37 +13,40 @@ def get_translation_matrix(t):
 
 
 def get_rotation_x_axis(theta):
-    theta = radians(theta)
+    theta = np.radians(theta)
     return np.array(
         [[1, 0, 0, 0],
-         [0, cos(theta), -sin(theta), 0],
-         [0, sin(theta), cos(theta), 0],
-         [0, 0, 0, 1]]
-    )
+         [0, np.cos(theta), -np.sin(theta), 0],
+         [0, np.sin(theta), np.cos(theta), 0],
+         [0, 0, 0, 1]], dtype=np.float32)
 
 
 def get_rotation_y_axis(theta):
-    theta = radians(theta)
+    theta = np.radians(theta)
     return np.array(
-        [[cos(theta), 0, sin(theta), 0],
+        [[np.cos(theta), 0, np.sin(theta), 0],
          [0, 1, 0, 0],
-         [-sin(theta), 0, cos(theta), 0],
-         [0, 0, 0, 1]]
-    )
+         [-np.sin(theta), 0, np.cos(theta), 0],
+         [0, 0, 0, 1]], dtype=np.float32)
 
 
 def get_rotation_z_axis(theta):
-    theta = radians(theta)
+    theta = np.radians(theta)
     return np.array(
-        [[cos(theta), -sin(theta), 0, 0],
-         [sin(theta), cos(theta), 0, 0],
+        [[np.cos(theta), -np.sin(theta), 0, 0],
+         [np.sin(theta), np.cos(theta), 0, 0],
          [0, 0, 1, 0],
-         [0, 0, 0, 1]]
-    )
+         [0, 0, 0, 1]], dtype=np.float32)
 
 
 if __name__ == "__main__":
-    point = np.array([[1], [1], [1], [1]])
-    print(np.matmul(get_rotation_x_axis(90), point))
-    print(np.matmul(get_rotation_y_axis(90), point))
-    print(np.matmul(get_rotation_z_axis(90), point))
+    print('Test for rotation from 0 to 90 degree angle')
+    point_x = np.array([[1], [0], [1], [1]])
+    point_y = np.array([[0], [1], [1], [1]])
+    point_z = np.array([[1], [0], [1], [1]])
+    x, y, z = (point_x, point_y, point_z)
+    for i in range(10, 100, 10):
+        x = np.append(x, np.matmul(get_rotation_x_axis(i), point_x), axis=1)
+        y = np.append(y, np.matmul(get_rotation_y_axis(i), point_y), axis=1)
+        z = np.append(z, np.matmul(get_rotation_z_axis(i), point_z), axis=1)
+    print('x axis\n', x, '\ny axis\n', y, '\nz axis\n', z)